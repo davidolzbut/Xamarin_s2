db2fc7a56d281d1a8c840f62984a3060ce4fd051
davidolzbut <david.olzbut@sps-prosek.cz>
2020-09-23 16:07:44 +0200

    23.9.

--- Xamarin_s2/MainPage.xaml.cs
+++ Xamarin_s2/MainPage.xaml.cs
@@ -13,6 +13,23 @@ namespace Xamarin_s2
         public MainPage()
         {
             InitializeComponent();
+
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            }
+        }
+        void mujSlider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            mujLabel.Text = String.Format("Hodnota je {0:F2}", e.NewValue);
+            mujLabel.Opacity = e.NewValue;
+            mujButton.Opacity = e.NewValue;
+            mujEntry.Opacity = e.NewValue;
+        }
+
+        void mujButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Nová zpráva!", mujEntry.Text, "OK");
         }
     }
 }
